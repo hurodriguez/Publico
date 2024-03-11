2b4c4b6a4eb4b070644c1ccc07571edbabe3d83e
Hugo Rodríguez <hurodriguez@outlook.com>
2024-03-11 09:50:21 -0400

    Update horario.js

--- horario.js
+++ horario.js
@@ -3,66 +3,121 @@
     "Nombre": "IVELISSE DÍAZ S.",
     "Cedula": "00107510455",
     "Email_personal": "",
-    "Email_institucional": "20166301@ce.pucmm.edu.do",
+    "Email_institucional": "dirprueba@pucmm.edu.do",
     "Telefono": "",
     "Celular": "809/890-1493",
     "Estado": "ACTIVO",
-    "Ultimo_Ciclo_Activo": "1850-Septiembre de 2023",
+    "Ultimo_Ciclo_Activo": "1820-Septiembre de 2022",
     "FACULTAD": [
         {
             "Tipo_docencia": "Profesor Por Asignatura CSTA",
             "Facultad_docencia": "FCH",
             "Escuela_Docencia": "Escuela de Lenguas",
-            "Clase": 1392,
-            "Cod_Asignatura": "    LET101",
-            "Descripcion_Clase": "Español I",
-            "Horario": "12-5, L, Mi",
-            "Aula/Edificio": "SDB1609",
-            "Campus": "CSTA"
+            "Clase": 2471,
+            "Cod_Asignatura": "    LET125",
+            "Descripcion_Clase": "Procesos de la Producción Escr",
+            "HORARIO": [
+                {
+                    "DIA": "L",
+                    "HORA": "09-12",
+                    "ID_INSTALACION": "SDB1303",
+                    "AULA": "SD-B1-303",
+                    "DESCRIPCION": "B1-303",
+                    "ID_EDIFICIO": "SD-B1",
+                    "DESCR_EDIFICIO": "Aulas B1",
+                    "CAMPUS": "CSTA",
+                    "LATITUD": 0,
+                    "LONGITUD": 0
+                }
+            ]
         },
         {
             "Tipo_docencia": "Profesor Por Asignatura CSTA",
             "Facultad_docencia": "FCH",
             "Escuela_Docencia": "Escuela de Lenguas",
-            "Clase": 1403,
-            "Cod_Asignatura": "    LLI211",
-            "Descripcion_Clase": "Comprensión y Producción Oral",
-            "Horario": "14-17, Mi",
-            "Aula/Edificio": "SDB1312",
-            "Campus": "CSTA"
-        },
-        {
-            "Tipo_docencia": "Profesor Por Asignatura CSTA",
-            "Facultad_docencia": "FCH",
-            "Escuela_Docencia": "Escuela de Lenguas",
-            "Clase": 1725,
+            "Clase": 2823,
             "Cod_Asignatura": "    LET102",
             "Descripcion_Clase": "Español II",
-            "Horario": "08-10, L, Mi",
-            "Aula/Edificio": "SDB1309",
-            "Campus": "CSTA"
+            "HORARIO": [
+                {
+                    "DIA": "L  Mi",
+                    "HORA": "10-12",
+                    "ID_INSTALACION": "SDB1306B",
+                    "AULA": "SD-B1-306B",
+                    "DESCRIPCION": "B1-306-B",
+                    "ID_EDIFICIO": "SD-B1",
+                    "DESCR_EDIFICIO": "Aulas B1",
+                    "CAMPUS": "CSTA",
+                    "LATITUD": 0,
+                    "LONGITUD": 0
+                }
+            ]
         },
         {
             "Tipo_docencia": "Profesor Por Asignatura CSTA",
             "Facultad_docencia": "FCH",
-            "Escuela_Docencia": "Decanato de Postgrado",
-            "Clase": 6596,
-            "Cod_Asignatura": "    ELA613",
-            "Descripcion_Clase": "Corrientes Act. Enseñ. Lengua",
-            "Horario": "11-13, S",
-            "Aula/Edificio": "",
-            "Campus": "CSTA"
+            "Escuela_Docencia": "Escuela de Lenguas",
+            "Clase": 3014,
+            "Cod_Asignatura": "    LET418",
+            "Descripcion_Clase": "Historia de la Lengua Española",
+            "HORARIO": [
+                {
+                    "DIA": "L",
+                    "HORA": "14-17",
+                    "ID_INSTALACION": "SDB1108",
+                    "AULA": "SD-B1-108",
+                    "DESCRIPCION": "B1-108",
+                    "ID_EDIFICIO": "SD-B1",
+                    "DESCR_EDIFICIO": "Aulas B1",
+                    "CAMPUS": "CSTA",
+                    "LATITUD": 0,
+                    "LONGITUD": 0
+                }
+            ]
         },
         {
             "Tipo_docencia": "Profesor Por Asignatura CSTA",
             "Facultad_docencia": "FCH",
-            "Escuela_Docencia": "Decanato de Postgrado",
-            "Clase": 6605,
-            "Cod_Asignatura": "    ELA613",
-            "Descripcion_Clase": "Corrientes Act. Enseñ. Lengua",
-            "Horario": "13-15, S",
-            "Aula/Edificio": "",
-            "Campus": "CSTA"
+            "Escuela_Docencia": "Escuela de Lenguas",
+            "Clase": 3032,
+            "Cod_Asignatura": "    LET416",
+            "Descripcion_Clase": "Redacción Académica y Profesio",
+            "HORARIO": [
+                {
+                    "DIA": "L  Mi",
+                    "HORA": "08-10",
+                    "ID_INSTALACION": "SDB1107",
+                    "AULA": "SD-B1-107",
+                    "DESCRIPCION": "B1-107",
+                    "ID_EDIFICIO": "SD-B1",
+                    "DESCR_EDIFICIO": "Aulas B1",
+                    "CAMPUS": "CSTA",
+                    "LATITUD": 0,
+                    "LONGITUD": 0
+                }
+            ]
+        },
+        {
+            "Tipo_docencia": "Profesor Por Asignatura CSTA",
+            "Facultad_docencia": "FCH",
+            "Escuela_Docencia": "Escuela de Lenguas",
+            "Clase": 6220,
+            "Cod_Asignatura": "    LET101",
+            "Descripcion_Clase": "Español I",
+            "HORARIO": [
+                {
+                    "DIA": "L  Mi",
+                    "HORA": "12-14",
+                    "ID_INSTALACION": "SDB104A",
+                    "AULA": "SD-B1-04A",
+                    "DESCRIPCION": "BI-04A Aula Octagonal",
+                    "ID_EDIFICIO": "SD-B1",
+                    "DESCR_EDIFICIO": "Aulas B1",
+                    "CAMPUS": "CSTA",
+                    "LATITUD": 0,
+                    "LONGITUD": 0
+                }
+            ]
         }
     ]
 }
