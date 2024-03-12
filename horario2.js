2b61d81df741b2e26bdd1154f0d042447ac9f7c2
Hugo Rodríguez <hurodriguez@outlook.com>
2024-03-12 09:53:55 -0400

    Create horario2.js

---
+++ horario2.js
@@ -0,0 +1,123 @@
+{
+    "ID_profesor": "00007658",
+    "Nombre": "IVELISSE DÍAZ S.",
+    "Cedula": "00107510455",
+    "Email_personal": "",
+    "Email_institucional": "dirprueba@pucmm.edu.do",
+    "Telefono": "",
+    "Celular": "809/890-1493",
+    "Estado": "ACTIVO",
+    "Ultimo_Ciclo_Activo": "1820-Septiembre de 2022",
+    "FACULTAD": [
+        {
+            "Tipo_docencia": "Profesor Por Asignatura CSTA",
+            "Facultad_docencia": "FCH",
+            "Escuela_Docencia": "Escuela de Lenguas",
+            "Clase": 2471,
+            "Cod_Asignatura": "    LET125",
+            "Descripcion_Clase": "Procesos de la Producción Escrita",
+            "HORARIO": [
+                {
+                    "DIA": "L",
+                    "HORA": "09-12",
+                    "ID_INSTALACION": "SDB1303",
+                    "AULA": "SD-B1-303",
+                    "DESCRIPCION": "B1-303",
+                    "ID_EDIFICIO": "SD-B1",
+                    "DESCR_EDIFICIO": "Aulas B1",
+                    "CAMPUS": "CSTA",
+                    "LATITUD": 0,
+                    "LONGITUD": 0
+                }
+            ]
+        },
+        {
+            "Tipo_docencia": "Profesor Por Asignatura CSTA",
+            "Facultad_docencia": "FCH",
+            "Escuela_Docencia": "Escuela de Lenguas",
+            "Clase": 2823,
+            "Cod_Asignatura": "    LET102",
+            "Descripcion_Clase": "Español II",
+            "HORARIO": [
+                {
+                    "DIA": "L  Mi",
+                    "HORA": "10-12",
+                    "ID_INSTALACION": "SDB1306B",
+                    "AULA": "SD-B1-306B",
+                    "DESCRIPCION": "B1-306-B",
+                    "ID_EDIFICIO": "SD-B2",
+                    "DESCR_EDIFICIO": "Aulas B1",
+                    "CAMPUS": "CSTA",
+                    "LATITUD": 0,
+                    "LONGITUD": 0
+                }
+            ]
+        },
+        {
+            "Tipo_docencia": "Profesor Por Asignatura CSTA",
+            "Facultad_docencia": "FCH",
+            "Escuela_Docencia": "Escuela de Lenguas",
+            "Clase": 3014,
+            "Cod_Asignatura": "    LET418",
+            "Descripcion_Clase": "Historia de la Lengua Española",
+            "HORARIO": [
+                {
+                    "DIA": "L",
+                    "HORA": "14-17",
+                    "ID_INSTALACION": "SDB1108",
+                    "AULA": "SD-B1-108",
+                    "DESCRIPCION": "B1-108",
+                    "ID_EDIFICIO": "SD-B3",
+                    "DESCR_EDIFICIO": "Aulas B1",
+                    "CAMPUS": "CSTA",
+                    "LATITUD": 0,
+                    "LONGITUD": 0
+                }
+            ]
+        },
+        {
+            "Tipo_docencia": "Profesor Por Asignatura CSTA",
+            "Facultad_docencia": "FCH",
+            "Escuela_Docencia": "Escuela de Lenguas",
+            "Clase": 3032,
+            "Cod_Asignatura": "    LET416",
+            "Descripcion_Clase": "Redacción Académica y Profesio",
+            "HORARIO": [
+                {
+                    "DIA": "L  Mi",
+                    "HORA": "08-10",
+                    "ID_INSTALACION": "SDB1107",
+                    "AULA": "SD-B1-107",
+                    "DESCRIPCION": "B1-107",
+                    "ID_EDIFICIO": "SD-B4",
+                    "DESCR_EDIFICIO": "Aulas B1",
+                    "CAMPUS": "CSTA",
+                    "LATITUD": 0,
+                    "LONGITUD": 0
+                }
+            ]
+        },
+        {
+            "Tipo_docencia": "Profesor Por Asignatura CSTA",
+            "Facultad_docencia": "FCH",
+            "Escuela_Docencia": "Escuela de Lenguas",
+            "Clase": 6220,
+            "Cod_Asignatura": "    LET101",
+            "Descripcion_Clase": "Español I",
+            "HORARIO": [
+                {
+                    "DIA": "L  Mi",
+                    "HORA": "12-14",
+                    "ID_INSTALACION": "SDB104A",
+                    "AULA": "SD-B1-04A",
+                    "DESCRIPCION": "BI-04A Aula Octagonal",
+                    "ID_EDIFICIO": "SD-B5",
+                    "DESCR_EDIFICIO": "Aulas B1",
+                    "CAMPUS": "CSTA",
+                    "LATITUD": 0,
+                    "LONGITUD": 0
+                }
+            ]
+        }
+    ]
+}
